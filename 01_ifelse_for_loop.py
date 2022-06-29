1675cec8cfbc7b8dbf65a05fcdefdccae87d03a9
urvashi2410 <urvashianand0906@gmail.com>
2022-06-29 16:18:43 +0530

    class-1 if else, for loop, range function

---
+++ 01_ifelse_for_loop.py
@@ -0,0 +1,146 @@
+# print(''' good ''')
+
+# comments 
+# = single line comment
+# multiline comment = 
+''' 
+ docstring
+ jsjds
+
+
+ dsdsdh
+'''
+# variables 
+# int = number 0........ no decimal 
+# float = decimal numbers 
+# boolean = true/false 
+# string  = multiple chars 
+# char = single character 
+
+# a = '34'
+# b = '56'
+# print(a+b)
+
+# conditional statement 
+# age = 12
+
+# typecast = int()
+# age = int(input('Enter your age'))
+# if age == 18:
+#     print('go and apply for voter id card')
+# elif age > 18:
+#     print('you can vote')
+# else:
+#     print('you cannot vote')
+
+
+# if 1 < 2:
+#     print('1 > 2')
+# if 3 > 0:
+#     print('3 == 0')
+# if 4 <= 9:
+#     print('4 >= 9')
+# if 1 < 2:
+#     print('True')
+# elif 2 != 4:
+#     print('2 != 4')
+# elif 5 > 4:
+#     print('5 > 4')
+# else:
+#     print('Default')
+
+# if else elif 
+# if = single execute ho jata hai 
+# elif = it always occurs after if  
+# else = after if 
+# if/elif = possible 
+# if/else = possible 
+# if-if-if-if = it will check all ifs 
+# if-elif-else = only one will be executed 
+# if-else / if/elif = only one will be executed 
+
+# np = numpy 
+# pandas = pd 
+
+# nested conditional statements 
+
+# age = 23
+# isVoter = True   
+
+# if age == 18:
+#     if isVoter == True:
+#         print('you can vote')
+# elif age > 18:
+#     if isVoter == True:
+#         print('You can vote elif ')
+#     print('You cannot vote elif')
+# else:
+#     print('You cannot vote')
+
+
+# loops 
+
+
+# def function():
+#     pass 
+
+# loops 
+# for loop 
+# print(1)
+# print(2)
+
+# range(1, 10)
+# startpoint = 1 = included 
+# endpoint = 10 = excluded 
+# 1 - 9 
+
+# range(0, 10)
+# 0 - 9
+
+# range(0, 10, 2)
+# 0 2 4 6 8 
+
+# i = 0- by default 
+
+
+# for i in range(10, 0, -3):
+#     print(i)
+
+# steps = by default = 1 
+# for i in range(1, 11):
+#     print(i)
+
+
+# single  = endpoint 
+# for i in range(11):
+#     print(i)
+
+# i = 10 
+# 10 - 11 
+
+# nothing will be printed 
+# for i in range(10, 3):
+#     print(i)
+
+# for i in range(1, 10, -3):
+#     print(i)
+
+
+# s = 'absfsdkfhksdhf'
+
+# string 
+# for i in s:
+#     print(i)
+
+# l = [11,12,3,4,5,5,6,7,8]
+# i = 1, 2, 
+
+# for i in l:
+#     print(i)
+
+# l[2] = 
+
+# i = 0, 1, ...... 8
+# l[i] = l[0] = 11 
+# for i in range(len(l)):
+#     print(l[i])
